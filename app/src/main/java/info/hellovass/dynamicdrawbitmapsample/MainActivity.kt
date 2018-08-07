c9e124dfe54b51ef49ad531ea3d39ec2c7508191
hellovass <hellova33@gmail.com>
2018-08-07 09:51:19 +0800

    kotlin支持

---
+++ app/src/main/java/info/hellovass/dynamicdrawbitmapsample/MainActivity.kt
@@ -0,0 +1,12 @@
+package info.hellovass.dynamicdrawbitmapsample
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.acitivty_main)
+    }
+}
